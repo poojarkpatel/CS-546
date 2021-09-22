4a7fd6815bdf814940355de0c1605b9b23104a9e
poojarkpatel <43582429+poojarkpatel@users.noreply.github.com>
2021-09-22 19:27:14 -0400

    Add files via upload

--- arrayUtils.js
+++ arrayUtils.js
@@ -1,5 +1,3 @@
-const { isDeepEqual } = require("./objUtils");
-
 function mean(array){
     let sum = 0;
     if(array === undefined) throw 'No inputs passed';
@@ -19,11 +17,11 @@ function medianSquared(array) {
     if(!(array.every((val) => typeof val === "number"))) throw 'The elements in array are not of number type';
     if (array.length % 2 == 0){
         let index = (array.length)/2;
-        return ((array[index] + array[index - 1])/2)**2;
+        return (array[index] + array[index - 1])/2;
     };
     if (array.length % 2 != 0){
         let index = (array.length)/2;
-        return (array[Math.floor(index)])**2;
+        return array[Math.floor(index)];
     };
 }
 
@@ -31,7 +29,7 @@ function maxElement(array){
     if(array === undefined) throw 'No inputs passed';
     if (!(Array.isArray(array))) throw 'The parameter is not Array type';
     if (array.length === 0) throw 'The array is empty';
-     
+    if(!(array.every((val) => typeof val === "number"))) throw 'The elements in array are not of number type';
     let maxElement = array[0];
     let maxElementObj = {};
     for (let i = 0; i < array.length; i++) {
@@ -46,8 +44,7 @@ function maxElement(array){
 function fill(end, value) {
     let fillArray = [];
     if (end <= 0) throw'The number should be greater than zero and a positive number';
-    if(end === undefined) throw  'No values passed';
-    if(typeof end !== "number" && value===undefined) throw 'No values passed';
+    if(end === undefined && value == undefined) throw  'No values passed';
     if (!(typeof end == "number")) throw 'the value must be the number type';
     for(let i=0; i< end; i++){
         if (value === undefined){
@@ -61,131 +58,48 @@ function fill(end, value) {
 }
 
 function countRepeating(array){
-    let emptyObj = {};
-    if(array === undefined) throw 'No inputs passed';
-    if (!(Array.isArray(array))) throw 'The parameter is not Array type';
-    if (array.length === 0) return emptyObj;
-    if(!(array.every((val) => (typeof val === "number") || (typeof val === "string")))) throw 'The elements in array are not of number or string type';
-    resultValue = {};
-    result = {};
-    for (let i=0; i<array.length; i++){
-        if (Object.keys(resultValue).includes(array[i].toString())){
-            resultValue[array[i]] += 1;
-        }
-        else{
-            resultValue[array[i]] = 1;
-        }
-    }
-    for(let i = 0; i< array.length; i++){
-        if (resultValue[array[i]] >= 2){
-            result[array[i]] = resultValue[array[i]]
-        }
-    }
-    return result;
+
 }
+
 function isEqual(arrayOne, arrayTwo){
-    if(arrayOne === undefined || arrayTwo === undefined) throw 'No inputs passed';
-    if (!(Array.isArray(arrayOne)) || !(Array.isArray(arrayTwo))) throw 'The parameter is not Array type';
-    if(arrayOne.length === 0 && arrayTwo.length ===0) return true;
-    let result = false;
-    if (arrayOne.length !== arrayTwo.length){
-        return false;
-    } else{
-        arrayOne.sort();
-        arrayTwo.sort();
-        for(let i = 0; i < arrayOne.length; i++){
-            if (Array.isArray(arrayOne[i]) && Array.isArray(arrayTwo[i])){
-                result = isEqual(arrayOne[i], arrayTwo[i])
-                if (result === false){
-                    return false;
-                }
-            } else {
-                if (arrayOne[i] === arrayTwo[i]){
-                    result = true;
-                }
-                else{
-                    return false;
-                }
-            }
-        }  
-    }
-    return result;
+    
 }
-   
-//     const arraycomp = function arraycomp(arrayOne,arrayTwo){
 
-//         if(arrayOne.length !== arrayTwo.length) {
-//            result = false;
-//            return false;
-//         }
-        
-        
-//             for (i=0; i<arrayOne.length; i++)
-//             {
-//                 arrayOne.sort();
-//                 arrayTwo.sort();
-//                 if (arrayOne[i] !== arrayTwo[i])
-//                 {
-//                     result = false;
-//                 }
-//                 else
-//                 {
-//                     result = true;
-//                 }
-//             }
-//     }
+function camelCase(string){
+    if (string === undefined) throw 'No input passed';
+    if(string.length < 0) throw 'string length is less than 0';
+    if(!(typeof string == "string")) throw 'Not string type';
+    let splittedValuesArray = string.split(" ");
     
-//     const nestedarr = function nestedarr(arrayOne,arrayTwo){
-//         // console.log(arrayOne,arrayTwo)
-//         if(arrayOne.length !== arrayTwo.length) { 
-//             result = false;
-//         }
-         
-//         for(let i =0; i<arrayOne.length; i++)
-//         {
-//                 arraycomp(arrayOne[i], arrayTwo[i]);
-//                 if(result==false){
-//                     break;
-//                 }
-                
-//         }
-        
-//     }
 
-//     for(i=0; i<arrayOne.length; i++)
-//     {
-//         if(Array.isArray(arrayOne[i]) && Array.isArray(arrayTwo[i])) 
-//         {
-//             nestedarr(arrayOne,arrayTwo);
-//             break;
-//         }
-//         else
-//         {
-//             arraycomp(arrayOne,arrayTwo);
-//             break;
-//         }
-//     }
-//     return result
+
     
-// }
+}
 
+function replaceChar(string){
 
+}
+
+function mashUp(string1, string2){
+
+}
+
+function makeArrays([objects]){
+
+}
+
+function isDeepEqual(obj1, obj2){
+
+}
+
+function computeObject(object, func){
+
+}
 
 module.exports = {
     mean,
     medianSquared,
     maxElement,
-    fill,
-    countRepeating,
-    isEqual
+    fill, 
+    camelCase
 };
-
-
-// console.log(isEqual([1, 2, 3], [3, 1, 2])); // Returns: true
-// console.log(isEqual([ 'Z', 'R', 'B', 'C', 'A' ], ['R', 'B', 'C', 'A', 'Z'])); // Returns: true
-// console.log(isEqual([1, 2, 3], [4, 5, 6])); // Returns: false
-// console.log(isEqual([1, 3, 2], [1, 2, 3, 4])); // Returns: false
-// console.log(isEqual([1, 2], [1, 2, 3])); // Returns: false
-// console.log(isEqual([[ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ]], [[ 3, 1, 2 ], [ 5, 4, 6 ], [ 9, 7,7,7, 8 ]])); // Returns: true
-// console.log(isEqual([[ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ]], [[ 3, 1, 2 ], [ 5, 4, 11 ], [ 9, 7, 8 ]])); // Returns: false
-// console.log(isEqual([null, null, null], [null, null, null])); // Returns: true
